58aa45fea6b2a878ac9a6a313a55f528a6acd7eb
thimarsola <tmarsola94@gmail.com>
2021-12-09 15:31:41 -0300

    conclusion of products page

--- page.php
+++ page.php
@@ -11,6 +11,8 @@ if(is_page('Sobre nós')){
     get_template_part('template-parts/pages/pages', 'pillars');
     get_template_part('template-parts/home/home', 'certification');
     get_template_part('template-parts/pages/pages', 'companies');
+}elseif(is_page('Nossos Produtos')){
+    get_template_part('template-parts/home/home', 'products');
 }
 
 ?>
